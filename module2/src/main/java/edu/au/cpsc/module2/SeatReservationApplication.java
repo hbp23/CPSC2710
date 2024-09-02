7cadb9e584f4630a731d602a17c1ddefff14bad1
Harsh <141373266+hbp23@users.noreply.github.com>
2024-09-01 22:40:01 -0500

    created Base for GUI for SeatReservationApp

--- module2/src/main/java/edu/au/cpsc/module2/SeatReservationApplication.java
+++ module2/src/main/java/edu/au/cpsc/module2/SeatReservationApplication.java
@@ -3,6 +3,12 @@ package edu.au.cpsc.module2;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.*;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -10,14 +16,53 @@ import java.io.IOException;
 public class SeatReservationApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(SeatReservationApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        HBox bottomButtons = createHBox();
+        GridPane infoPane = createGridPane();
+        Background backgroundColor = createBackgroundColor();
+
+        BorderPane root = new BorderPane(infoPane, null, null, bottomButtons, null);
+        root.setBackground(backgroundColor);
+
+        Scene scene = new Scene(root, 300, 300);
         stage.setScene(scene);
+        stage.setTitle("Seat Reservation Application");
         stage.show();
     }
 
-    public static void main(String[] args) {
-        launch();
+    private Background createBackgroundColor() {
+        BackgroundFill backgroundFill = new BackgroundFill(Color.LIGHTGRAY, null, null);
+        return new Background(backgroundFill);
     }
+
+    private HBox createHBox() {
+        HBox bottomButtons = new HBox();
+        Button cancelButton = new Button("Cancel");
+        Button saveButton = new Button("Save");
+        bottomButtons.getChildren().addAll(cancelButton, saveButton);
+        return bottomButtons;
+    }
+
+    private GridPane createGridPane() {
+        GridPane infoPane = new GridPane();
+
+        Label flightDesignatorLabel = new Label("Flight Designator:");
+        infoPane.add(flightDesignatorLabel, 0, 0);
+        Label flightDateLabel = new Label("Flight Date:");
+        infoPane.add(flightDateLabel, 0, 1);
+        Label firstNameLabel = new Label("First Name:");
+        infoPane.add(firstNameLabel, 0, 2);
+        Label lastNameLabel = new Label("Last Name:");
+        infoPane.add(lastNameLabel, 0, 3);
+        Label bagsLabel = new Label("Bags:");
+        infoPane.add(bagsLabel, 0, 4);
+        Label flyingWithInfantLabel = new Label("Flying with infant?");
+        infoPane.add(flyingWithInfantLabel, 0, 5);
+        Label numOfPassengers = new Label("Number of passengers:");
+        infoPane.add(numOfPassengers, 0, 6);
+
+
+
+        return  infoPane;
+    }
+
 }
