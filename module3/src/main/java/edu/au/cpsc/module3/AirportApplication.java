a8393f590532fe288cea978752a01dda6182ba88
Harsh <141373266+hbp23@users.noreply.github.com>
2024-09-06 17:10:50 -0500

    Data User Interface Progress

--- module3/src/main/java/edu/au/cpsc/module3/AirportApplication.java
+++ module3/src/main/java/edu/au/cpsc/module3/AirportApplication.java
@@ -11,8 +11,8 @@ public class AirportApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(AirportApplication.class.getResource("Airport-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        Scene scene = new Scene(fxmlLoader.load());
+        stage.setTitle("Airport Locator");
         stage.setScene(scene);
         stage.show();
     }
--- module3/src/main/java/edu/au/cpsc/module3/AirportController.java
+++ module3/src/main/java/edu/au/cpsc/module3/AirportController.java
@@ -1,14 +1,49 @@
 package edu.au.cpsc.module3;
 
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+import javafx.scene.web.WebView;
 
-public class AirportController {
-    @FXML
-    private Label welcomeText;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class AirportController implements Initializable {
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private HBox searchTools;
+
+    @FXML
+    private VBox airportInfo;
+
+    @FXML
+    private TextField nameTextField;
+
+    @FXML
+    private TextField typeTextField;
+
+    @FXML
+    private TextField cityTextField;
+
+    @FXML
+    private TextField elevationTextField;
+
+    @FXML
+    private TextField countryTextField;
+
+    @FXML
+    private TextField regionTextField;
+
+    @FXML
+    private TextField municipalityTextField;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
     }
 }
