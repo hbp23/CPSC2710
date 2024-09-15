8d970f9bad69b5057070d25a19ff0604d24940a8
Harsh <141373266+hbp23@users.noreply.github.com>
2024-09-15 16:16:38 -0500

    created i-vars, getters and setters for it, unit tests for them

---
+++ module4/src/main/java/edu/au/cpsc/module4/ScheduledFlight.java
@@ -0,0 +1,95 @@
+package edu.au.cpsc.module4;
+
+import java.time.LocalTime;
+import java.util.HashSet;
+
+public class ScheduledFlight {
+
+    // fields
+    private String flightDesignator;
+    private String departureAirportIdent;
+    private java.time.LocalTime departureTime;
+    private String arrivalAirportIdent;
+    private java.time.LocalTime arrivalTime;
+    private HashSet<String> daysOfWeek;
+
+    // constructor
+
+
+    public ScheduledFlight(String flightDesignator, String departureAirportIdent, LocalTime departureTime, String arrivalAirportIdent, LocalTime arrivalTime, HashSet<String> daysOfWeek) {
+        this.flightDesignator = flightDesignator;
+        this.departureAirportIdent = departureAirportIdent;
+        this.departureTime = departureTime;
+        this.arrivalAirportIdent = arrivalAirportIdent;
+        this.arrivalTime = arrivalTime;
+        this.daysOfWeek = daysOfWeek;
+    }
+
+    public ScheduledFlight() {}
+
+    public String getFlightDesignator() {
+        return flightDesignator;
+    }
+
+    public void setFlightDesignator(String flightDesignator) {
+        if (flightDesignator == null) {
+            throw new IllegalArgumentException("flightDesignator cannot be null");
+        }
+        this.flightDesignator = flightDesignator;
+    }
+
+    public String getDepartureAirportIdent() {
+        return departureAirportIdent;
+    }
+
+    public void setDepartureAirportIdent(String departureAirportIdent) {
+        if (departureAirportIdent == null) {
+            throw new IllegalArgumentException("departureAirportIdent cannot be null");
+        }
+        this.departureAirportIdent = departureAirportIdent;
+    }
+
+    public LocalTime getDepartureTime() {
+        return departureTime;
+    }
+
+    public void setDepartureTime(LocalTime departureTime) {
+        if (departureTime == null) {
+            throw new IllegalArgumentException("departureTime cannot be null");
+        }
+        this.departureTime = departureTime;
+    }
+
+    public String getArrivalAirportIdent() {
+        return arrivalAirportIdent;
+    }
+
+    public void setArrivalAirportIdent(String arrivalAirportIdent) {
+        if (arrivalAirportIdent == null) {
+            throw new IllegalArgumentException("arrivalAirportIdent cannot be null");
+        }
+        this.arrivalAirportIdent = arrivalAirportIdent;
+    }
+
+    public LocalTime getArrivalTime() {
+        return arrivalTime;
+    }
+
+    public void setArrivalTime(LocalTime arrivalTime) {
+        if (arrivalTime == null) {
+            throw new IllegalArgumentException("arrivalTime cannot be null");
+        }
+        this.arrivalTime = arrivalTime;
+    }
+
+    public HashSet<String> getDaysOfWeek() {
+        return daysOfWeek;
+    }
+
+    public void setDaysOfWeek(HashSet<String> daysOfWeek) {
+        if (daysOfWeek == null) {
+            throw new IllegalArgumentException("daysOfWeek cannot be null");
+        }
+        this.daysOfWeek = daysOfWeek;
+    }
+}
---
+++ module4/src/test/java/edu/au/cpsc/module4/ScheduledFlightTest.java
@@ -0,0 +1,82 @@
+package edu.au.cpsc.module4;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalTime;
+import java.util.Arrays;
+import java.util.HashSet;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ScheduledFlightTest {
+
+    private ScheduledFlight f1;
+    @BeforeEach
+    void setUp() {
+        f1 = new ScheduledFlight();
+    }
+
+    @Test
+    void givenNewScheduledFlightThenGettersReturnConstructorArguments() {
+        String[] s = new String[] {"Monday", "Tuesday", "Wednesday"};
+        HashSet<String> h = new HashSet<>(Arrays.asList(s));
+        f1 = new ScheduledFlight("DL1331", "KPIT", LocalTime.of(8, 0, 15), "PPIT", LocalTime.of(8, 0, 15), h);
+        assertEquals("DL1331", f1.getFlightDesignator());
+        assertEquals("KPIT", f1.getDepartureAirportIdent());
+        assertEquals(LocalTime.of(8, 0, 15), f1.getDepartureTime());
+        assertEquals("PPIT", f1.getArrivalAirportIdent());
+        assertEquals(LocalTime.of(8, 0, 15), f1.getArrivalTime());
+        assertEquals(h, f1.getDaysOfWeek());
+    }
+
+    @Test
+    void givenNewScheduledFlightWhenFlightDesignatorSettersUsedThenGettersReturnSettersArguments() {
+        f1.setFlightDesignator("DL1331");
+        assertEquals("DL1331", f1.getFlightDesignator());
+    }
+
+    @Test
+    void givenNewScheduledFlightWhenDepartureAirportIdentSetterUsedThenGettersReturnSettersArguments() {
+        f1.setDepartureAirportIdent("KPIT");
+        assertEquals("KPIT", f1.getDepartureAirportIdent());
+    }
+
+    @Test
+    void givenNewScheduledFlightWhenDepartureTimeSetterUsedThenGettersReturnSettersArguments() {
+        f1.setDepartureTime(LocalTime.of(8, 0, 15));
+        assertEquals(LocalTime.of(8, 0, 15), f1.getDepartureTime());
+    }
+
+    @Test
+    void givenNewScheduledFlightWhenArrivalAirportIdentSetterUsedThenGettersReturnSettersArguments() {
+        f1.setArrivalAirportIdent("PPIT");
+        assertEquals("PPIT", f1.getArrivalAirportIdent());
+    }
+
+    @Test
+    void givenNewScheduledFlightWhenArrivalTimeSetterUsedThenGettersReturnSettersArguments() {
+        f1.setArrivalTime(LocalTime.of(8, 0, 15));
+        assertEquals(LocalTime.of(8, 0, 15), f1.getArrivalTime());
+    }
+
+    @Test
+    void givenNewScheduledFlightWhenDaysOfWeekSetterUsedThenGettersReturnSettersArguments() {
+        String[] s = new String[] {"Monday", "Tuesday", "Wednesday"};
+        HashSet<String> h = new HashSet<>(Arrays.asList(s));
+        f1.setDaysOfWeek(h);
+        assertEquals(h, f1.getDaysOfWeek());
+    }
+
+    @Test
+    void givenNewScheduledFlightWhenSettersAreNullThenThrowsIllegalArgumentException() {
+        assertThrows(IllegalArgumentException.class, () -> f1.setFlightDesignator(null));
+        assertThrows(IllegalArgumentException.class, () -> f1.setDepartureAirportIdent(null));
+        assertThrows(IllegalArgumentException.class, () -> f1.setDepartureTime(null));
+        assertThrows(IllegalArgumentException.class, () -> f1.setArrivalAirportIdent(null));
+        assertThrows(IllegalArgumentException.class, () -> f1.setArrivalTime(null));
+        assertThrows(IllegalArgumentException.class, () -> f1.setDaysOfWeek(null));
+    }
+
+
+}
