ee6989b191fa033a7ccec1628c2f747926ea64a6
Harsh <141373266+hbp23@users.noreply.github.com>
2024-09-17 01:10:43 -0500

    finished with bug fix, my fx: includes were glitched no code was changed, i just had to remove them from sceneBuilder and add them back for the program to initialize the tableview and detail view straight from the controllers.

--- module4/src/main/java/edu/au/cpsc/module4/FlightScheduleApplication.java
+++ module4/src/main/java/edu/au/cpsc/module4/FlightScheduleApplication.java
@@ -11,7 +11,7 @@ public class FlightScheduleApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
 
-        FXMLLoader fxmlLoader = new FXMLLoader(FlightScheduleApplication.class.getResource("/app/flightScheduleApplication-view.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(FlightScheduleApplication.class.getResource("/flightScheduleApplication-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
         stage.setTitle("Flight Schedule Application");
         stage.setScene(scene);
